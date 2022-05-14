b7fcfc30d4e5691cd2d7228a5cf5a9e0290a5a43
Julie Basile <hello@jebasile.com>
2022-05-14 13:15:59 -0400

    commit - images + background

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+/* CREATING FUNCTION FOR IMAGE SIMPLICITY */
+function newImage(url, left, bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+/* 
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+}
+
+function newItem('dblclick', )
+
+object.addEventListener('dblclick', () =>{
+    object.remove()
+});
+*/
+
+/* ADDING SKY + GRASS */
+function title(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++) {
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+let horizon = window.innerHeight / 2
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+title('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+title('assets/grass.png', 0, 0, window.innerWidth/100, heightOfSky/100)
+
+
+/* ADDING IMAGES */
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newImage('assets/sword.png', 500, 405)
+
+newImage('assets/sheild.png', 165, 185)
+newImage('assets/staff.png', 600, 100)
+
+newImage('assets/clouds3.png', 120, 640)
+newImage('assets/clouds.png', 100, 600)
+newImage('assets/clouds2.png', 10, 620)
