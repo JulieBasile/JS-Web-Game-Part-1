5ed07c53fe238820678df71d38f7c29607c34fd1
Julie Basile <hello@jebasile.com>
2022-05-16 15:58:35 -0400

    commit 2 - dblclick addEventListener

--- index.js
+++ index.js
@@ -9,17 +9,13 @@ function newImage(url, left, bottom) {
     return object
 }
 
-/* 
+/* EVENT LISTENER TO REMOVE OBJECTS */
 function newItem(url, left, bottom){
     let object = newImage(url, left, bottom)
-}
-
-function newItem('dblclick', )
-
-object.addEventListener('dblclick', () =>{
+    object.addEventListener('dblclick', () => {
     object.remove()
-});
-*/
+    })
+}
 
 /* ADDING SKY + GRASS */
 function title(url, left, bottom, width, height){
@@ -38,19 +34,17 @@ title('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
 title('assets/grass.png', 0, 0, window.innerWidth/100, heightOfSky/100)
 
 
-/* ADDING IMAGES */
+/* ADDING IMAGES + ITEMS*/
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
-
-newImage('assets/sword.png', 500, 405)
-
-newImage('assets/sheild.png', 165, 185)
-newImage('assets/staff.png', 600, 100)
-
 newImage('assets/clouds3.png', 120, 640)
 newImage('assets/clouds.png', 100, 600)
 newImage('assets/clouds2.png', 10, 620)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
